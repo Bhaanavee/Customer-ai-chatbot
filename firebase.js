96534c3d1460dcdce7ead99b5112c9063e5034bc
Bhaanavee <Bhanavee123@gmail.com>
2024-09-29 21:43:28 +0530

    final

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider} from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgWlKG-iMCFWCWtfQ-MSzJUn522L9DJgI",
-  authDomain: "pawtopia-71026.firebaseapp.com",
-  projectId: "pawtopia-71026",
-  storageBucket: "pawtopia-71026.appspot.com",
-  messagingSenderId: "873347534504",
-  appId: "1:873347534504:web:a07b7b1d72963e79b3bc9d",
-  measurementId: "G-RXW0SW3FJR"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
